d802ab9e0da1aa53b48cb9b3af03c36373b03cbf
Brody Schmidt <mbs139@case.edu>
2018-04-12 10:25:42 -0400

    Added JUnit Testers

---
+++ Backend/Java Project Files/src/MeetPredictTester.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MeetPredictTester {
+
+	@Test
+	void testDefaultMeet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTaperMeet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHomeMeet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHomeTaperMeet() {
+		fail("Not yet implemented");
+	}
+
+}
